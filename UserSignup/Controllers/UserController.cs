ddacd773ee7c0b4203a847a394159d2caf02876e
Anthony Becerra <abece005@fiu.edu>
2017-10-05 18:33:07 -0400

    Added basic functionality.

--- UserSignup/Controllers/UserController.cs
+++ UserSignup/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using UserSignup.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -15,5 +16,26 @@ namespace UserSignup.Controllers
         {
             return View();
         }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(User user, string verify)
+        {
+            if ( user.Password == verify )
+            {
+                return View("Index");
+            }
+
+            else
+            {
+                ViewBag.user = user;
+                return View("Add");
+            }
+        }
     }
+
 }
--- UserSignup/Models/User.cs
+++ UserSignup/Models/User.cs
@@ -7,6 +7,18 @@ namespace UserSignup.Models
 {
     public class User
     {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
 
+        public User(string username, string email, string password)
+        {
+            Username = username;
+            Email = email;
+            Password = password;
+        }
+
+        // default constructor
+        public User() { } 
     }
 }
